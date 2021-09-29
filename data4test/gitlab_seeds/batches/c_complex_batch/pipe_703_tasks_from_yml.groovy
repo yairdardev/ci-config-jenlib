7e49cda5e7c577dbb503ac9c620de0b763d352ed
ydmeta lincloud <ydmeta@yairdar.com>
2021-09-29 23:50:10 +0000

    prismo:import.gitrepo.dir[git@github.com:ydzvulon/jenlib.git]/data4test/gitlab_seeds].mirror

---
+++ data4test/gitlab_seeds/batches/c_complex_batch/pipe_703_tasks_from_yml.groovy
@@ -0,0 +1,29 @@
+
+// @Library('JenkinsLib_jenlib') _
+
+library identifier: 'jenlib@0.7.5', retriever: http(
+credentialsId: 'asd',
+httpURL: 'http://httpit:8043/__localbuild__/jenbuild/jenlib.zip')
+
+def kwj = [
+    'scmvars': null,
+    'task_parse_result': [:],
+]
+
+node {
+
+    stage('start'){
+        sh 'echo START'
+        kwj.scmvars = checkout scm
+    }
+
+    dir('data4test/gitlab_seeds/batches/c_complex_batch'){
+        jen.step_stages_from_tasks(
+            kwj, '.' ,'Taskfile.yml', 'ci-flow'
+        )
+    }
+
+    stage('finis'){
+        sh 'echo FINSH'
+    }
+}
